c43f5ff102b6e807e21de74ca8c1d3d65fde1f19
Imraan-Hendricks <imraan.hndx@gmail.com>
2022-09-27 23:17:13 +0200

    Created button component

---
+++ src/components/ui/Button.tsx
@@ -0,0 +1,28 @@
+import styled from '@emotion/styled';
+import { Color } from '../../theme/types';
+
+export const Button = styled.button<{ color?: Color }>(
+  ({ theme, color = 'primary' }) => ({
+    ...theme.typography.button,
+    color: theme.palette[color].contrast[600],
+    backgroundColor: theme.palette[color][600],
+    borderRadius: '.375rem',
+    cursor: 'pointer',
+    display: 'inline-block',
+    padding: '.75rem 1.75rem',
+    textAlign: 'center',
+    transition: theme.transition.colors,
+
+    '&:hover': {
+      color: theme.palette[color].contrast[700],
+      backgroundColor: theme.palette[color][700],
+      boxShadow: theme.shadows[2],
+    },
+
+    '&:active': {
+      color: theme.palette[color].contrast[500],
+      backgroundColor: theme.palette[color][500],
+      boxShadow: theme.shadows[0],
+    },
+  })
+);
