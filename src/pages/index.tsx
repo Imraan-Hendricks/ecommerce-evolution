d03e77c407a935830c6653f59d1ceec7e1aa4edb
Imraan-Hendricks <imraan.hndx@gmail.com>
2022-10-14 00:04:30 +0200

    Created pricing section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,14 @@ import { Hero } from '../components/marketing/Hero';
 import { Layout } from '../components/layout/Layout';
 import { NextPage } from 'next';
 import { Overview } from '../components/marketing/Overview';
+import { Pricing } from '../components/marketing/Pricing';
 
 const Home: NextPage = () => (
   <Layout lean title='Home'>
     <Hero />
     <Features />
     <Overview />
+    <Pricing />
   </Layout>
 );
 
