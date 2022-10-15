120215c6bdb1b866315119391e0342fe2303e0e1
Imraan-Hendricks <imraan.hndx@gmail.com>
2022-10-15 02:29:20 +0200

    Updated typography demo

--- src/components/demo/TypographyDemo.tsx
+++ src/components/demo/TypographyDemo.tsx
@@ -1,6 +1,7 @@
 import styled from '@emotion/styled';
+import { Box } from '../ui/Box';
 import { Container } from '../ui/Container';
-import { FC, PropsWithChildren } from 'react';
+import { FC, PropsWithChildren, useState } from 'react';
 import { Typography } from '../ui/Typography';
 
 const paragaph = `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
@@ -8,56 +9,78 @@ const paragaph = `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
                   consectetur, neque doloribus, cupiditate numquam dignissimos laborum
                   fugiat deleniti? Eum quasi quidem quibusdam.`;
 
-export const TypographyDemo: FC = () => (
-  <Module>
-    <Layout>
-      <Title>Typography</Title>
-      <TwoColumn>
+export const TypographyDemo: FC = () => {
+  const [isBold, setIsBold] = useState(false);
+  return (
+    <Module>
+      <Layout>
+        <Title>Typography</Title>
+        <Box>
+          <BoldSelector bold={!isBold} onClick={() => setIsBold(false)}>
+            Normal
+          </BoldSelector>
+          <VerticalTextDivider />
+          <BoldSelector bold={isBold} onClick={() => setIsBold(true)}>
+            Bold
+          </BoldSelector>
+        </Box>
         <Layout>
-          <H1>H1. Heading One</H1>
-          <H2>H2. Heading Two</H2>
-          <H3>H3. Heading Three</H3>
-          <H4>H4. Heading Four</H4>
-          <H5>H5. Heading Five</H5>
-          <H6>H6. Heading Six</H6>
+          <H1 bold={isBold}>H1. Heading One</H1>
+          <H2 bold={isBold}>H2. Heading Two</H2>
+          <H3 bold={isBold}>H3. Heading Three</H3>
+          <H4 bold={isBold}>H4. Heading Four</H4>
+          <H5 bold={isBold}>H5. Heading Five</H5>
+          <H6 bold={isBold}>H6. Heading Six</H6>
+          <Paragaph bold={isBold} variant='subtitle1'>
+            {paragaph}
+          </Paragaph>
+          <Paragaph bold={isBold} variant='subtitle2'>
+            {paragaph}
+          </Paragaph>
+          <Paragaph bold={isBold} variant='body1'>
+            {paragaph}
+          </Paragaph>
+          <Paragaph bold={isBold} variant='body2'>
+            {paragaph}
+          </Paragaph>
+          <Button bold={isBold}>Button Text</Button>
+          <Caption bold={isBold}>caption text</Caption>
+          <Overline bold={isBold}>OVERLINE TEXT</Overline>
         </Layout>
-        <Layout>
-          <Paragaph variant='subtitle1'>{paragaph}</Paragaph>
-          <Paragaph variant='subtitle2'>{paragaph}</Paragaph>
-          <Paragaph variant='body1'>{paragaph}</Paragaph>
-          <Paragaph variant='body2'>{paragaph}</Paragaph>
-          <Button>Button Text</Button>
-          <Caption>caption text</Caption>
-          <Overline>OVERLINE TEXT</Overline>
-        </Layout>
-      </TwoColumn>
-    </Layout>
-  </Module>
-);
+      </Layout>
+    </Module>
+  );
+};
 
 const Module = styled.section({ padding: '6rem 1.25rem' });
 
-const Layout = styled(Container)({ display: 'grid', gap: '2rem' });
-
-const Title = styled(Typography)(({ theme }) => ({
-  textAlign: 'center',
-  [theme.breakpoints.up('2xl')]: {
-    textAlign: 'start',
-  },
-}));
-Title.defaultProps = { as: 'h1', variant: 'h4' };
-
-const TwoColumn = styled.div(({ theme }) => ({
+const Layout = styled(Container)({
   display: 'grid',
-  gridTemplateColumns: 'repeat(1, minmax(0, 1fr))',
   gap: '2rem',
   textAlign: 'center',
-  margin: 'auto auto',
-  [theme.breakpoints.up('2xl')]: {
-    gridTemplateColumns: 'repeat(2, minmax(0, 1fr))',
-    textAlign: 'start',
-  },
-}));
+});
+
+const Title = styled(Typography)({
+  textAlign: 'center',
+});
+Title.defaultProps = { as: 'h1', variant: 'h4' };
+
+const BoldSelector = styled(Typography)<{ bold?: boolean }>(
+  ({ theme, bold }) => ({
+    color: bold ? theme.palette.black[500] : theme.palette.gray[500],
+    '&:hover': {
+      color: bold ? theme.palette.black[500] : theme.palette.gray[700],
+    },
+  })
+);
+BoldSelector.defaultProps = { as: 'button', variant: 'button' };
+
+const VerticalTextDivider = styled(Typography)({ margin: '0 1rem' });
+VerticalTextDivider.defaultProps = {
+  as: 'span',
+  children: '|',
+  variant: 'button',
+};
 
 const H1 = styled(Typography)();
 H1.defaultProps = { as: 'h1', variant: 'h1' };
@@ -79,12 +102,17 @@ H6.defaultProps = { as: 'h6', variant: 'h6' };
 
 const Paragaph: FC<
   PropsWithChildren<{
+    bold?: boolean;
     variant: 'subtitle1' | 'subtitle2' | 'body1' | 'body2';
   }>
-> = ({ children, variant }) => (
-  <Typography as='p' css={{ maxWidth: '48rem' }} variant={variant}>
+> = ({ bold, children, variant }) => (
+  <Typography
+    as='p'
+    css={{ maxWidth: '48rem', margin: '0 auto' }}
+    bold={bold}
+    variant={variant}>
     {variant}.{' '}
-    <Typography as='span' color='gray' variant={variant}>
+    <Typography as='span' color='gray' bold={bold} variant={variant}>
       {children}
     </Typography>
   </Typography>
