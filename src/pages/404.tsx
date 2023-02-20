36844e899857c67a21aaf4f776bab76f20ac920f
Imraan-Hendricks <imraan.hndx@gmail.com>
2023-02-20 02:39:54 +0200

    Created 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,10 @@
+import { Box } from '@/components/ui/Box';
+import { NotFound } from '@/components/marketing/NotFound';
+
+export default function () {
+  return (
+    <Box as='main'>
+      <NotFound />
+    </Box>
+  );
+}
